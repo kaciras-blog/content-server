c4cf9dd2d4c257252df9562598cebb4ca1337270
Kaciras <kaciras@outlook.com>
2018-09-12 14:35:21 +0800

    why git missing 2 files to commit?

---
+++ src/main/java/net/kaciras/blog/api/article/ArticleListRequest.java
@@ -0,0 +1,25 @@
+package net.kaciras.blog.api.article;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.kaciras.blog.api.DeletedState;
+import org.springframework.data.domain.Pageable;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class ArticleListRequest {
+
+	private Pageable pageable;
+
+	private int userId;
+
+	private Integer category;
+
+	/**
+	 * 资源的删除状态过滤，对于无删除状态的资源，此字段将被忽略
+	 */
+	@NotNull
+	private DeletedState deletion = DeletedState.FALSE;
+}
