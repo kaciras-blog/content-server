0d4b1892f0c092fd63c52e3f3b15a1c6290d5563
Kaciras <kaciras@outlook.com>
2018-09-19 16:01:12 +0800

    ? ? ?...

--- src/main/java/net/kaciras/blog/api/user/UserDao.java
+++ src/main/java/net/kaciras/blog/api/user/UserDao.java
@@ -3,18 +3,18 @@ package net.kaciras.blog.api.user;
 import org.apache.ibatis.annotations.*;
 
 @Mapper
-interface UserDao {
+interface UserDAO {
 
-	@Insert("INSERT INTO User(name,head) VALUES(#{name},#{head})")
+	@Insert("INSERT INTO `user`(name,head) VALUES(#{name},#{head})")
 	@Options(useGeneratedKeys = true, keyColumn = "id")
 	void insert(User user);
 
-	@Select("SELECT id,name,head,deleted FROM User WHERE id=#{id}")
+	@Select("SELECT id,name,head,deleted FROM `user` WHERE id=#{id}")
 	User select(int id);
 
-	@Select("SELECT id,name,deleted FROM User WHERE name=#{name}")
+	@Select("SELECT id,name,deleted FROM `user` WHERE name=#{name}")
 	User selectByName(String name);
 
-	@Delete("UPDATE User SET deleted=1 WHERE id=#{id}")
+	@Delete("UPDATE `user` SET deleted=1 WHERE id=#{id}")
 	int delete(int id);
 }
