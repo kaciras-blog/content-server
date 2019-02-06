c4addb42a939347a34015e39b966c0af369c7c2b
Kaciras <kaciras@outlook.com>
2019-02-06 14:26:51 +0800

    UserModule: remove ban support

--- src/main/java/net/kaciras/blog/api/user/User.java
+++ src/main/java/net/kaciras/blog/api/user/User.java
@@ -4,11 +4,6 @@ import lombok.*;
 import net.kaciras.blog.infrastructure.codec.ImageRefrence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.lang.Nullable;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -17,11 +12,6 @@ import java.util.List;
 public class User {
 
 	public static final User GUEST = new User(0, "游客");
-
-	@Autowired
-	@Getter(AccessLevel.NONE)
-	@Setter(AccessLevel.NONE)
-	private BanRecordDAO banRecordDAO;
 
 	@Autowired
 	@Getter(AccessLevel.NONE)
@@ -41,40 +31,8 @@ public class User {
 		this.name = name;
 	}
 
-	@Nullable
-	public LocalDateTime getBannedEndTime() {
-		return banRecordDAO.selectLastEndTime(id);
-	}
-
-	public List<BanRecord> getBanRecords() {
-		return banRecordDAO.selectBanRecords(id);
-	}
-
 	public void updateHead(ImageRefrence head) {
 		this.head = head;
 		userDAO.updateHead(head);
-	}
-
-	/**
-	 * 封禁该用户
-	 *
-	 * @param operator 操作者id
-	 * @param time     封禁时间
-	 * @param cause    封禁原因描述
-	 * @return 封禁记录的id
-	 */
-	int ban(int operator, Duration time, String cause) {
-		var start = LocalDateTime.now();
-		var record = new BanRecord()
-				.setStart(start)
-				.setEnd(start.plus(time))
-				.setOperator(operator)
-				.setCause(cause);
-		banRecordDAO.insertBanRecord(id, record);
-		return record.getId();
-	}
-
-	void unBan(int bid, int operator, String cause) {
-		banRecordDAO.insertUnbanRecord(bid, operator, cause);
 	}
 }
--- src/main/java/net/kaciras/blog/api/user/UserController.java
+++ src/main/java/net/kaciras/blog/api/user/UserController.java
@@ -2,13 +2,8 @@ package net.kaciras.blog.api.user;
 
 import lombok.RequiredArgsConstructor;
 import net.kaciras.blog.infrastructure.principal.RequireAuthorize;
-import net.kaciras.blog.infrastructure.principal.SecurityContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.time.Duration;
-import java.util.List;
 
 @RequireAuthorize // 仅管理员
 @RequiredArgsConstructor
@@ -22,33 +17,6 @@ class UserController {
 	@GetMapping("/{id}")
 	public UserVo get(@PathVariable int id) {
 		return userManager.getUser(id);
-	}
-
-	@PostMapping("/{id}/ban-records")
-	public ResponseEntity ban(@PathVariable int id,
-							  @RequestParam(required = false, defaultValue = "0") long time,
-							  @RequestParam String cause) {
-		var bid = repository.get(id)
-				.ban(SecurityContext.getUserId(), Duration.ofSeconds(time), cause);
-
-		var location = String.format("/users/%d/banRecords/%d", id, bid);
-		return ResponseEntity.created(URI.create(location)).build();
-	}
-
-	@PostMapping("/{id}/banRecords/{bid}/undoRecord")
-	public ResponseEntity unban(@PathVariable int id,
-								@PathVariable int bid,
-								@RequestParam String cause) {
-		repository.get(id)
-				.unBan(bid, SecurityContext.getUserId(), cause);
-
-		var location = String.format("/users/%d/banRecords/%d/undoRecord", id, bid);
-		return ResponseEntity.created(URI.create(location)).build();
-	}
-
-	@GetMapping("/{id}/banRecords")
-	public List<BanRecord> getBanRecords(@PathVariable int id) {
-		return repository.get(id).getBanRecords();
 	}
 
 	@PatchMapping("/{id}")
