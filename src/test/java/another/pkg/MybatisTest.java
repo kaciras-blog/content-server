4e8021e8cb716bdc9ddeb63fb8e03b1be2f38565
Kaciras <kaciras@outlook.com>
2019-02-17 16:52:23 +0800

    test mybatis databinding of boolean

---
+++ src/test/java/another/pkg/MybatisTest.java
@@ -0,0 +1,23 @@
+package another.pkg;
+
+import org.apache.ibatis.binding.BindingException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+final class MybatisTest {
+
+	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+	@Autowired
+	private TestMybatisMapper mapper;
+
+	/** 该测试验证了Mybatis不会把空值视作false */
+	@Test
+	void testSelectNullableBoolean() {
+		Assertions.assertThatThrownBy(mapper::selectNullableBool).isInstanceOf(BindingException.class);
+	}
+}
