071715a2651636b3fd6cac007cc9dbe0f74e81a0
Kaciras <Kaciras@outlook.com>
2019-04-24 15:48:11 +0800

    config: fix bean definition retrieve

--- src/main/java/net/kaciras/blog/api/config/ConfigService.java
+++ src/main/java/net/kaciras/blog/api/config/ConfigService.java
@@ -1,12 +1,13 @@
 package net.kaciras.blog.api.config;
 
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -14,76 +15,92 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
+
 @RequiredArgsConstructor
 @Service
-public class ConfigService implements BeanPostProcessor, InitializingBean {
+public class ConfigService implements BeanPostProcessor, ApplicationContextAware {
 
-	private final BeanDefinitionRegistry beanRegistry;
 	private final ConfigStore configStore;
 
 	// 无需使用线程安全的Map，因为它仅在启动时修改
 	private final Map<String, ChangeListener> listenerMap = new HashMap<>();
 
+	private BeanDefinitionRegistry beanRegistry;
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.beanRegistry = (BeanDefinitionRegistry) context;
+	}
+
 	public Object postProcessBeforeInitialization(Object bean, String beanName) {
 		var clazz = bean.getClass();
 
-		// 只绑定单例 bean，因为没法知道原型 bean 什么时候销毁从而解绑
-		var scope = beanRegistry.getBeanDefinition(beanName).getScope();
-		if (!ConfigurableBeanFactory.SCOPE_PROTOTYPE.equals(scope)) {
+		if (!clazz.getName().startsWith("net.kaciras")) {
 			return bean;
 		}
 
-		// method binding
-		for (var method : clazz.getDeclaredMethods()) {
-			var bind = method.getDeclaredAnnotation(ConfigBind.class);
-			if (bind == null) {
-				continue;
-			}
-			method.setAccessible(true);
-
-			var parameterTypes = method.getParameterTypes();
-			if (parameterTypes.length != 1) {
-				throw new BeanInitializationException("绑定的方法必须只有一个参数：" + beanName + "，方法：" + method);
-			}
-
-			var type = parameterTypes[0];
-			var listener = listenerMap.computeIfAbsent(bind.value(), __ -> new ChangeListener(type));
-			if (listener.getType() != type) {
-				throw new BeanInitializationException("绑定的配置类型不一致：" + beanName + "，方法：" + method);
-			}
-
-			listener.add(value -> method.invoke(bean, value));
+		// TODO: beanName 过滤第三方依赖，和(inner bean)#xxxx
+		String scope;
+		try {
+			scope = beanRegistry.getBeanDefinition(beanName).getScope();
+		} catch (NoSuchBeanDefinitionException e) {
+			return bean;
 		}
 
-		for (var field : clazz.getDeclaredFields()) {
-			var bind = field.getDeclaredAnnotation(ConfigBind.class);
-			if (bind == null) {
-				continue;
-			}
-			field.setAccessible(true);
+		// 只绑定单例 bean，因为没法知道原型 bean 什么时候销毁从而解绑
+		if (SCOPE_SINGLETON.equals(scope) || "".equals(scope)) {
+			// method binding
+			for (var method : clazz.getDeclaredMethods()) {
+				var bind = method.getDeclaredAnnotation(ConfigBind.class);
+				if (bind == null) {
+					continue;
+				}
+				method.setAccessible(true);
 
-			var type = field.getType();
-			var listener = listenerMap.computeIfAbsent(bind.value(), __ -> new ChangeListener(type));
-			if (listener.getType() != type) {
-				throw new BeanInitializationException("绑定的配置类型不一致：" + beanName + "，字段：" + field);
+				var parameterTypes = method.getParameterTypes();
+				if (parameterTypes.length != 1) {
+					throw new BeanInitializationException("绑定的方法必须只有一个参数：" + beanName + "，方法：" + method);
+				}
+
+				var type = parameterTypes[0];
+				var listener = listenerMap.computeIfAbsent(bind.value(), __ -> new ChangeListener(type));
+				if (listener.getType() != type) {
+					throw new BeanInitializationException("绑定的配置类型不一致：" + beanName + "，方法：" + method);
+				}
+
+				listener.add(value -> method.invoke(bean, value));
 			}
 
-			listener.add(value -> field.set(bean, value));
+			for (var field : clazz.getDeclaredFields()) {
+				var bind = field.getDeclaredAnnotation(ConfigBind.class);
+				if (bind == null) {
+					continue;
+				}
+				field.setAccessible(true);
+
+				var type = field.getType();
+				var listener = listenerMap.computeIfAbsent(bind.value(), __ -> new ChangeListener(type));
+				if (listener.getType() != type) {
+					throw new BeanInitializationException("绑定的配置类型不一致：" + beanName + "，字段：" + field);
+				}
+
+				listener.add(value -> field.set(bean, value));
+			}
 		}
-
 		return bean;
 	}
 
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		for (var prop : configStore) {
-			var listener = listenerMap.get(prop.key);
-			if (listener != null) {
-				listener.fire(prop.value);
-			}
-		}
-	}
+	// TODO: 这个在 postProcessBeforeInitialization 之前调用
+//	@Override
+//	public void afterPropertiesSet() throws Exception {
+//		for (var prop : configStore) {
+//			var listener = listenerMap.get(prop.key);
+//			if (listener != null) {
+//				listener.fire(prop.value);
+//			}
+//		}
+//	}
 
 	public <T> T get(String name, Class<T> type, T defau1t) {
 		var value = configStore.load(Collections.singletonList(name)).get(name);
