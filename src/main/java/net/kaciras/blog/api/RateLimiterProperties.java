2102b4fb50ad8303937e4da7d8d6d1596f2bc8b8
Kaciras <Kaciras@outlook.com>
2019-05-20 10:30:38 +0800

    tweak rate limitation

--- src/main/java/net/kaciras/blog/api/RateLimiterProperties.java
+++ src/main/java/net/kaciras/blog/api/RateLimiterProperties.java
@@ -10,11 +10,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public final class RateLimiterProperties {
 
 	/** 允许的速率（令牌/秒）*/
-	private double rate = 1.5;
+	private double rate = 2;
 
 	/** 令牌桶容量，同时也是单位时间能获取的上限（令牌）*/
-	private int bucketSize = 10;
+	private int bucketSize = 16;
 
-	/** 令牌桶在Redis里保存的时间（秒）*/
+	/** 令牌桶在Redis里保存的时间（秒），至少要为：bucketSize / rate */
 	private int cacheTime = 60;
 }
