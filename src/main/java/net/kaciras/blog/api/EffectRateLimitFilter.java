cb902f16911f9474f7413bd9b6eec4abe07e4438
Kaciras <Kaciras@outlook.com>
2019-06-11 18:15:10 +0800

    EffectRateLimiter: response json data on reject & add refreshOnReject support

--- src/main/java/net/kaciras/blog/api/EffectRateLimitFilter.java
+++ src/main/java/net/kaciras/blog/api/EffectRateLimitFilter.java
@@ -1,6 +1,7 @@
 package net.kaciras.blog.api;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.kaciras.blog.infrastructure.Misc;
 import net.kaciras.blog.infrastructure.ratelimit.RateLimiter;
@@ -18,24 +19,26 @@ import java.util.concurrent.Executor;
 /**
  * Effect 指有副作用的请求，如提交评论等
  */
+@Setter
 @RequiredArgsConstructor
 @Slf4j
 @Order(Integer.MIN_VALUE + 20) // 比CORS过滤器低一点，比其他的高
 public class EffectRateLimitFilter extends AbstractRateLimitFilter {
 
-	private static final byte[] REJECT_MSG = "请求频率太快，你的IP被封禁1小时".getBytes(StandardCharsets.UTF_8);
+	private static final byte[] REJECT_MSG = "{\"message\":\"请求频率太快，IP被封禁\"}".getBytes(StandardCharsets.UTF_8);
 	private static final byte[] EMPTY = new byte[]{0};
 
 	private final RateLimiter rateLimiter;
 	private final RedisTemplate<String, byte[]> redisTemplate;
 	private final Executor threadPool;
 
-	private final Duration banTime = Duration.ofHours(1);
+	private Duration banTime = Duration.ofHours(1);
+	private boolean refreshOnReject;
 
 	/**
 	 * 异步统计请求频率：如果没有被封禁就请求成功，同时在另一个线程中获取限流器的令牌，如果达到限制则封禁，
 	 * 本次的请求仍然是成功的，封禁从下一个请求开始才生效。
-	 *
+	 * <p>
 	 * 这样做把对限流器的访问异步化，可以减少对请求的阻塞时间（一次Redis访问）而提高性能。（应该能提高一些吧我也没测）
 	 */
 	@Override
@@ -47,6 +50,9 @@ public class EffectRateLimitFilter extends AbstractRateLimitFilter {
 
 		if (Utils.nullableBool(redisTemplate.hasKey(blockKey))) {
 			reject(response);
+			if (refreshOnReject) {
+				redisTemplate.opsForValue().set(blockKey, EMPTY, banTime);
+			}
 			return false;
 		}
 
@@ -62,8 +68,8 @@ public class EffectRateLimitFilter extends AbstractRateLimitFilter {
 	}
 
 	private void reject(HttpServletResponse response) throws IOException {
-		response.setStatus(403);
-		response.setContentType("text/plain;charset=UTF-8");
+		response.setStatus(429);
+		response.setContentType("application/json;charset=UTF-8");
 		response.setContentLength(REJECT_MSG.length);
 		response.getOutputStream().write(REJECT_MSG);
 	}
