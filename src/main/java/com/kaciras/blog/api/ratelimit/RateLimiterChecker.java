a3378f50575206a467bdd944f311427f37550f01
Kaciras <Kaciras@protonmail.com>
2021-01-06 15:06:16 +0800

    add tests for RateLimitFilter

--- src/main/java/com/kaciras/blog/api/ratelimit/RateLimiterChecker.java
+++ src/main/java/com/kaciras/blog/api/ratelimit/RateLimiterChecker.java
@@ -3,7 +3,19 @@ package com.kaciras.blog.api.ratelimit;
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 
+@FunctionalInterface
 public interface RateLimiterChecker {
 
+	/**
+	 * 检查请求是否过于频繁，如果是则返回等待的时间，否则返回零。
+	 *
+	 * <h2>负数的对待方式</h2>
+	 * 负数的等待时间没有意义，如果是其它情况禁止的话请用 Filter 而不是该接口。
+	 * 所以不要返回负数，如果返回则后续的处理是未定义的。
+	 *
+	 * @param address 访问者的地址
+	 * @param request 请求
+	 * @return 等待的时间，零表示无需等待正常通过
+	 */
 	long check(InetAddress address, HttpServletRequest request);
 }
---
+++ src/test/java/com/kaciras/blog/api/ratelimit/RateLimitFilterTest.java
@@ -0,0 +1,67 @@
+package com.kaciras.blog.api.ratelimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.Filter;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+final class RateLimitFilterTest {
+
+	private final MockHttpServletResponse response = new MockHttpServletResponse();
+
+	private boolean accepted;
+
+	void doFilter(Filter filter, HttpServletRequest request) throws Exception {
+		filter.doFilter(request, response, (a, b) -> {
+			accepted = true;
+			response.setStatus(200);
+		});
+	}
+
+	@Test
+	void empty() throws Exception {
+		var filter = new RateLimitFilter(List.of());
+		var request = new MockHttpServletRequest();
+		request.setRemoteAddr("1234:6666::8888");
+
+		doFilter(filter, request);
+		assertThat(accepted).isTrue();
+	}
+
+	@Test
+	void allowLAN() throws Exception {
+		var checker = mock(RateLimiterChecker.class);
+		when(checker.check(any(), any())).thenReturn(100L);
+		var filter = new RateLimitFilter(List.of(checker));
+
+		var request = new MockHttpServletRequest();
+		request.setRemoteAddr("::1");
+
+		doFilter(filter, request);
+		assertThat(accepted).isTrue();
+	}
+
+	@Test
+	void reject() throws Exception {
+		var checker = mock(RateLimiterChecker.class);
+		when(checker.check(any(), any())).thenReturn(100L);
+		var filter = new RateLimitFilter(List.of(checker));
+
+		var request = new MockHttpServletRequest();
+		request.setRemoteAddr("1234:6666::8888");
+
+		doFilter(filter, request);
+
+		assertThat(accepted).isFalse();
+		assertThat(response.getStatus()).isEqualTo(429);
+		assertThat(response.getHeader("X-RateLimit-Wait")).isEqualTo("100");
+	}
+}
