51da48142b11f51b3fcd25525bc970eb7f30ba31
Kaciras <Kaciras@protonmail.com>
2021-02-25 15:37:20 +0800

    simplify AccountControllerTest

--- src/test/java/com/kaciras/blog/api/account/local/AccountControllerTest.java
+++ src/test/java/com/kaciras/blog/api/account/local/AccountControllerTest.java
@@ -1,13 +1,16 @@
 package com.kaciras.blog.api.account.local;
 
 import com.kaciras.blog.api.AbstractControllerTest;
+import com.kaciras.blog.api.MutateFactory;
 import com.kaciras.blog.api.SessionValue;
 import com.kaciras.blog.api.account.SessionService;
 import com.kaciras.blog.api.user.UserManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.mock.web.MockHttpSession;
@@ -34,51 +37,66 @@ final class AccountControllerTest extends AbstractControllerTest {
 	@MockBean
 	private SessionService sessionService;
 
+	@Autowired
+	private MutateFactory mutateFactory;
+
+	private final MockHttpSession session = new MockHttpSession();
+	private final SignupDTO signupDTO = new SignupDTO("alice", "foobar2000", "6666");
+	private final LoginDTO loginDTO = new LoginDTO("alice", "foobar2000", false);
+
+	@BeforeEach
+	void beforeEach() {
+		SessionValue.CAPTCHA.setTo(session, "6666");
+		SessionValue.CAPTCHA_TIME.setTo(session, Instant.now());
+	}
+
 	private static Stream<Arguments> invalidRegisterDTOs() {
 		var chs = new char[2000];
 		Arrays.fill(chs, 'p');
 		var longText = new String(chs);
 		return Stream.of(
-				Arguments.of(new SignupDTO(null, "foobar2000", "6666")),
-				Arguments.of(new SignupDTO("alice", null, "6666")),
-				Arguments.of(new SignupDTO("alice", "foobar2000", null)),
+				Arguments.of("name", null),
+				Arguments.of("password", null),
+				Arguments.of("captcha", null),
 
-				Arguments.of(new SignupDTO("", "foobar2000", "6666")),
-				Arguments.of(new SignupDTO(longText, "foobar2000", "6666")),
-				Arguments.of(new SignupDTO("♜♝♞♟", "foobar2000", "6666")),
+				Arguments.of("name", ""),
+				Arguments.of("name", longText),
+				Arguments.of("name", "♜♝♞♟"),
 
-				Arguments.of(new SignupDTO("alice", "1", "6666")),
-				Arguments.of(new SignupDTO("alice", longText, "6666"))
+				Arguments.of("password", "1"),
+				Arguments.of("password", longText)
 		);
 	}
 
 	@MethodSource("invalidRegisterDTOs")
 	@ParameterizedTest
-	void invalidSignupRequest(SignupDTO dto) throws Exception {
-		var session = new MockHttpSession();
-		SessionValue.CAPTCHA.setTo(session, "6666");
-		SessionValue.CAPTCHA_TIME.setTo(session, Instant.now());
+	void invalidSignupRequest(String field, Object value) throws Exception {
+		var body = mutateFactory.from(signupDTO).mutate(field, value).json();
 
-		mockMvc.perform(post("/accounts").content(toJson(dto)).session(session))
-				.andExpect(status().is(400));
+		mockMvc.perform(post("/accounts").content(body).session(session)).andExpect(status().is(400));
 	}
 
 	@Test
 	void signupWithoutCaptcha() throws Exception {
-		var dto = new SignupDTO("alice", "foobar2000", "6666");
-		mockMvc.perform(post("/accounts").content(toJson(dto)))
+		mockMvc.perform(post("/accounts").content(toJson(signupDTO)))
 				.andExpect(status().is(400))
 				.andExpect(jsonPath("$.message").value("验证码错误"));
 	}
 
 	@Test
 	void wrongCaptcha() throws Exception {
-		var dto = new SignupDTO("alice", "foobar2000", "6666");
-		var session = new MockHttpSession();
-		SessionValue.CAPTCHA.setTo(session, "6666");
+		SessionValue.CAPTCHA.setTo(session, "1111");
+
+		mockMvc.perform(post("/accounts").content(toJson(signupDTO)).session(session))
+				.andExpect(status().is(400))
+				.andExpect(jsonPath("$.message").value("验证码错误"));
+	}
+
+	@Test
+	void expiredCaptcha() throws Exception {
 		SessionValue.CAPTCHA_TIME.setTo(session, Instant.EPOCH);
 
-		mockMvc.perform(post("/accounts").content(toJson(dto)).session(session))
+		mockMvc.perform(post("/accounts").content(toJson(signupDTO)).session(session))
 				.andExpect(status().is(400))
 				.andExpect(jsonPath("$.message").value("验证码已过期，请重试"));
 	}
@@ -87,25 +105,14 @@ final class AccountControllerTest extends AbstractControllerTest {
 	void nameConflict() throws Exception {
 		when(userManager.createNew(any(), any(), any())).thenThrow(new DuplicateKeyException(""));
 
-		var dto = new SignupDTO("alice", "foobar2000", "6666");
-		var session = new MockHttpSession();
-		SessionValue.CAPTCHA.setTo(session, "6666");
-		SessionValue.CAPTCHA_TIME.setTo(session, Instant.now());
-
-		mockMvc.perform(post("/accounts").content(toJson(dto)).session(session))
-				.andExpect(status().is(400));
+		mockMvc.perform(post("/accounts").content(toJson(signupDTO)).session(session)).andExpect(status().is(400));
 	}
 
 	@Test
 	void signup() throws Exception {
 		when(userManager.createNew(any(), any(), any())).thenReturn(1);
 
-		var dto = new SignupDTO("alice", "foobar2000", "6666");
-		var session = new MockHttpSession();
-		SessionValue.CAPTCHA.setTo(session, "6666");
-		SessionValue.CAPTCHA_TIME.setTo(session, Instant.now());
-
-		mockMvc.perform(post("/accounts").content(toJson(dto)).session(session))
+		mockMvc.perform(post("/accounts").content(toJson(signupDTO)).session(session))
 				.andExpect(status().is(201));
 
 		verify(sessionService).putUser(notNull(), notNull(), eq(1), eq(true));
@@ -115,19 +122,17 @@ final class AccountControllerTest extends AbstractControllerTest {
 	void loginNonExists() throws Exception {
 		when(repository.findByName(any())).thenReturn(null);
 
-		var dto = new LoginDTO("alice", "foobar2000", false);
-		mockMvc.perform(post("/accounts/login").content(toJson(dto)))
+		mockMvc.perform(post("/accounts/login").content(toJson(loginDTO)))
 				.andExpect(status().is(400))
 				.andExpect(jsonPath("$.message").value("密码错误或用户不存在"));
 	}
 
 	@Test
 	void invalidPassword() throws Exception {
-		var account = Account.create(1, "alice", "foobar2000");
+		var account = Account.create(1, "alice", "12345678");
 		when(repository.findByName(any())).thenReturn(account);
 
-		var dto = new LoginDTO("alice", "12345678", false);
-		mockMvc.perform(post("/accounts/login").content(toJson(dto)))
+		mockMvc.perform(post("/accounts/login").content(toJson(loginDTO)))
 				.andExpect(status().is(400))
 				.andExpect(jsonPath("$.message").value("密码错误或用户不存在"));
 	}
@@ -137,8 +142,7 @@ final class AccountControllerTest extends AbstractControllerTest {
 		var account = Account.create(1, "alice", "foobar2000");
 		when(repository.findByName(any())).thenReturn(account);
 
-		var dto = new LoginDTO("alice", "foobar2000", false);
-		mockMvc.perform(post("/accounts/login").content(toJson(dto)))
+		mockMvc.perform(post("/accounts/login").content(toJson(loginDTO)))
 				.andExpect(status().is(201));
 
 		verify(sessionService).putUser(notNull(), notNull(), eq(1), eq(false));
